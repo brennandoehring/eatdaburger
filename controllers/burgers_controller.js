594c7ba0d7e42a621a03c38782f0a498f8f91c06
brennandoehring <nannerbdale@gmail.com>
2020-12-05 16:41:37 -0500

    Added devoured status and deleting burger from the database.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,4 +18,30 @@ router.post("/api/burgers", function (req, res) {
     });
 });
 
+// Sets burger devoured status
+router.put("/api/burgers/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
+
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// Delete a burger from the database
+router.delete("/api/burgers/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
+
+    burger.deleteOne(condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
 module.exports = router;
